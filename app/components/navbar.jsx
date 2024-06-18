9abbb9a388f8546f0541f9cb2214374fea224dcb
Kai Wang <wayy15308969733@163.com>
2024-06-18 12:44:53 +1000

    test v13

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
-          <Link href="/" className="text-3xl font-bold text-[#16f2b3]">
+          <Link href={`${basePath}/`} className="text-3xl font-bold text-[#16f2b3]">
             Daniel WANG
           </Link>
         </div>
@@ -16,27 +16,27 @@ function Navbar() {
           id="navbar-default"
         >
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#about`}>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience">
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#experience`}>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div>
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills">
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#skills`}>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div>
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education">
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#education`}>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div>
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects">
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#projects`}>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div>
             </Link>
           </li>
