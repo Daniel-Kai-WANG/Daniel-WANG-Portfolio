c97c14ae1c5b414dac42322e3cc40d58b771d328
Kai Wang <wayy15308969733@163.com>
2024-06-18 14:26:46 +1000

    test v16

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
           id="navbar-default"
         >
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href='/about'>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href='/#about'>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
