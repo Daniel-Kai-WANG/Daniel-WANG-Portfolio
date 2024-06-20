2ac177739e1b9661dfb86ce0a6f84076952dad49
Kai Wang <wayy15308969733@163.com>
2024-06-20 22:21:19 +1000

    test v25

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -7,10 +7,12 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
 
+const basePath = "/Daniel-WANG-Portfolio";
+
 function HeroSection() {
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
-      <Image src={"/hero.svg"} alt="Hero" width={1572} height={795} className="absolute -top-[98px] -z-10" />
+      <Image src={`${basePath}/hero.svg`} alt="Hero" width={1572} height={795} className="absolute -top-[98px] -z-10" />
 
       <div className="grid grid-cols-1 items-start gap-y-8 lg:grid-cols-2 lg:gap-12">
         <div className="order-2 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:order-1 lg:pt-10">
