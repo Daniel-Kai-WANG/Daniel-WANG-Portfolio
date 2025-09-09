e9864cbc9e525e435a391eb499598848cc9e09a8
Daniel-Kai-WANG <kaiwang2027@gmail.com>
2025-09-09 16:48:33 +1000

    try to fix svg buv

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  basePath: "/",
-  assetPrefix: "/",
+  basePath: isProd ? "/Daniel-WANG-Portfolio" : "",
+  assetPrefix: isProd ? "/Daniel-WANG-Portfolio/" : "",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
